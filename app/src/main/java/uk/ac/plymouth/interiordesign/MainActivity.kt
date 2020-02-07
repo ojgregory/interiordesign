f64029e427775523f8a5dfadf198cdec499e3f34
Oliver Gregory <oliver.gregory@students.plymouth.ac.uk>
2020-02-07 13:41:09 +0000

    Created basic application, does nothing more than show camera preview

--- app/src/main/java/uk/ac/plymouth/interiordesign/MainActivity.kt
+++ app/src/main/java/uk/ac/plymouth/interiordesign/MainActivity.kt
@@ -12,96 +12,23 @@ import android.os.HandlerThread
 import android.util.Log
 import android.view.TextureView
 import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
-import uk.ac.plymouth.interiordesign.MainActivity.SurfaceTextureListener as SurfaceTextureListener
+import uk.ac.plymouth.interiordesign.Fragments.CameraFragment
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class MainActivity : AppCompatActivity() {
-    private lateinit var cameraDevice : CameraDevice
-    private val cameraManager by lazy {
-        getSystemService(Context.CAMERA_SERVICE) as CameraManager
-    }
-
-    private lateinit var backgroundThread: HandlerThread
-    private lateinit var backgroundHandler: Handler
-
-    private fun startBackgroundThread() {
-        backgroundThread = HandlerThread("Camera2 Kotlin").also { it.start() }
-        backgroundHandler = Handler(backgroundThread.looper)
-    }
-
-    private fun stopBackgroundThread() {
-        backgroundThread.quitSafely()
-        try {
-            backgroundThread.join()
-        } catch (e: InterruptedException) {
-            Log.e(TAG, e.toString())
-        }
-    }
-
-    private val deviceStateCallback = object : CameraDevice.StateCallback() {
-        override fun onOpened(camera: CameraDevice) {
-            Log.d(TAG, "camera device opened")
-            cameraDevice = camera
-        }
-
-        override fun onDisconnected(camera: CameraDevice) {
-            Log.d(TAG, "camera device disconnected")
-            camera.close()
-        }
-
-        override fun onError(camera: CameraDevice, error: Int) {
-            Log.d(TAG, "camera device error")
-            finish()
-        }
-
-    }
-
-    private val surfaceListener = object: TextureView.SurfaceTextureListener {
-        override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture?, width: Int, height: Int) {
-        }
-
-        override fun onSurfaceTextureUpdated(surface: SurfaceTexture?) = Unit
-
-        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture?) = true
-
-        override fun onSurfaceTextureAvailable(surface: SurfaceTexture?, width: Int, height: Int) {
-            Log.d(TAG, "textureSurface width: $width height: $height")
-            openCamera()
-        }
-
-    }
-
-    companion object {
-        const val REQUEST_CAMERA_PERMISSION = 100
-        private val TAG = MainActivity::class.qualifiedName
-        @JvmStatic fun newInstance() = MainActivity()
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        startBackgroundThread()
-        if (textureview.isAvailable)
-            openCamera()
-        else
-            textureview.surfaceTextureListener = surfaceListener
-    }
-
-    override fun onPause() {
-
-        stopBackgroundThread()
-        super.onPause()
-    }
-
-    private fun openCamera() {
-        checkCameraPermission()
-    }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val cameraFragment = CameraFragment.newInstance()
+        addFragment(cameraFragment)
+    }
 
+    private fun addFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
     }
 }
