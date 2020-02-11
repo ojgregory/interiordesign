ead7c8941bf75324eded6f4e1c80769a343ccd62
Oliver Gregory <oliver.gregory@students.plymouth.ac.uk>
2020-02-11 16:17:50 +0000

    Fragments are now used to allow modularity of functionality

---
+++ app/src/main/java/uk/ac/plymouth/interiordesign/Fragments/CameraFragment.kt
@@ -0,0 +1,251 @@
+package uk.ac.plymouth.interiordesign.Fragments
+
+import android.Manifest
+import android.content.Context
+import android.graphics.ImageFormat
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.*
+import android.media.ImageReader
+import android.net.Uri
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.core.content.ContextCompat.getSystemService
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_camera.*
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
+import uk.ac.plymouth.interiordesign.MainActivity
+import uk.ac.plymouth.interiordesign.R
+import uk.ac.plymouth.interiordesign.SobelProcessor
+import java.util.*
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [CameraFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [CameraFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CameraFragment : Fragment() {
+    private val IMAGE_BUFFER_SIZE = 2
+    private val MAX_PREVIEW_WIDTH = 1280
+    private val MAX_PREVIEW_HEIGHT = 720
+    private lateinit var captureSession: CameraCaptureSession
+    private lateinit var captureRequestBuilder: CaptureRequest.Builder
+    private lateinit var cameraDevice : CameraDevice
+    private lateinit var imageReader : ImageReader
+    private val sobelProcessor : SobelProcessor = SobelProcessor()
+    /** [HandlerThread] where all buffer reading operations run */
+    private val imageReaderThread = HandlerThread("imageReaderThread").apply { start() }
+
+    /** [Handler] corresponding to [imageReaderThread] */
+    private val imageReaderHandler = Handler(imageReaderThread.looper)
+
+    private val deviceStateCallback = object: CameraDevice.StateCallback() {
+        override fun onOpened(camera: CameraDevice) {
+            Log.d(TAG, "camera device opened")
+            if (camera != null){
+                cameraDevice = camera
+                previewSession()
+            }
+
+        }
+
+        override fun onDisconnected(camera: CameraDevice) {
+            Log.d(TAG, "camera device disconnected")
+            camera?.close()
+        }
+
+        override fun onError(camera: CameraDevice, error: Int) {
+            Log.d(TAG, "camera device error")
+            this@CameraFragment.activity?.finish()
+        }
+
+    }
+
+    private val cameraManager by lazy {
+        activity?.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+    }
+
+    private lateinit var backgroundThread: HandlerThread
+    private lateinit var backgroundHandler: Handler
+
+    private fun previewSession() {
+        val surfaceTexture = textureview.surfaceTexture
+        surfaceTexture.setDefaultBufferSize(MAX_PREVIEW_WIDTH, MAX_PREVIEW_HEIGHT)
+        val surface = Surface(surfaceTexture)
+
+        // Initialize an image reader which will be used to apply filter to preview
+        val size = cameraCharacteristics(cameraId(CameraCharacteristics.LENS_FACING_BACK),
+            CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)!!
+            .getOutputSizes(ImageFormat.YUV_420_888).maxBy { it.height * it.width }!!
+        imageReader = ImageReader.newInstance(
+            size.width, size.height, ImageFormat.YUV_420_888, IMAGE_BUFFER_SIZE)
+
+        sobelProcessor.setOutputSurface(surface)
+
+        // Creates list of Surfaces where the camera will output frames
+        val targets = listOf(sobelProcessor.getInputNormalSurface())
+        imageReader.setOnImageAvailableListener(onImageReaderImageAvail(imageReader), imageReaderHandler)
+
+        captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+        captureRequestBuilder.addTarget(sobelProcessor.getInputNormalSurface())
+        //captureRequestBuilder.addTarget(imageReader.surface)
+
+
+        cameraDevice.createCaptureSession(
+            targets,
+            object: CameraCaptureSession.StateCallback(){
+                override fun onConfigureFailed(session: CameraCaptureSession) {
+                    Log.e(TAG, "creating capture session failed!")
+                }
+
+                override fun onConfigured(session: CameraCaptureSession) {
+                    if (session != null) {
+                        captureSession = session
+                        captureRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE)
+                        captureSession.setRepeatingRequest(captureRequestBuilder.build(), null, null)
+                    }
+                }
+
+            }, null)
+    }
+
+    private fun onImageReaderImageAvail(reader: ImageReader) = ImageReader.OnImageAvailableListener {
+        val image = reader.acquireLatestImage()
+        Log.d(TAG, "Image available: ${image.timestamp}")
+        image.close()
+    }
+
+    private fun closeCamera() {
+        if (this::captureSession.isInitialized)
+            captureSession.close()
+        if (this::cameraDevice.isInitialized)
+            cameraDevice.close()
+    }
+
+    private fun startBackgroundThread() {
+        backgroundThread = HandlerThread("Camera2 Kotlin").also { it.start() }
+        backgroundHandler = Handler(backgroundThread.looper)
+    }
+
+    private fun stopBackgroundThread() {
+        backgroundThread.quitSafely()
+        try {
+            backgroundThread.join()
+        } catch (e: InterruptedException) {
+            Log.e(TAG, e.toString())
+        }
+    }
+
+    private val surfaceListener = object: TextureView.SurfaceTextureListener {
+        override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture?, width: Int, height: Int) {
+        }
+
+        override fun onSurfaceTextureUpdated(surface: SurfaceTexture?) = Unit
+
+        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture?) = true
+
+        override fun onSurfaceTextureAvailable(surface: SurfaceTexture?, width: Int, height: Int) {
+            Log.d(TAG, "textureSurface width: $width height: $height")
+            openCamera()
+        }
+
+    }
+
+
+
+    private fun <T> cameraCharacteristics(cameraId: String, key: CameraCharacteristics.Key<T>) : T? {
+        val characteristics = cameraManager.getCameraCharacteristics(cameraId)
+        return when (key) {
+            CameraCharacteristics.LENS_FACING -> characteristics.get(key)
+            CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP -> characteristics.get(key)
+            else -> throw  IllegalArgumentException("Key not recognized")
+        }
+    }
+
+    private fun cameraId(lens: Int) : String {
+        var deviceId = listOf<String>()
+        try {
+            val cameraIdList = cameraManager.cameraIdList
+            deviceId = cameraIdList.filter { lens == cameraCharacteristics(it, CameraCharacteristics.LENS_FACING) }
+        } catch (e: CameraAccessException) {
+            Log.e(TAG, e.toString())
+        }
+        return deviceId[0]
+    }
+
+    private fun connectCamera() {
+        val deviceId = cameraId(CameraCharacteristics.LENS_FACING_BACK)
+        Log.d(TAG, "deviceId: $deviceId")
+        try {
+            cameraManager.openCamera(deviceId, deviceStateCallback, backgroundHandler)
+        } catch (e: CameraAccessException) {
+            Log.e(TAG, e.toString())
+        } catch (e: InterruptedException) {
+            Log.e(TAG, "Open camera device interrupted while opened")
+        }
+    }
+
+    companion object {
+        const val REQUEST_CAMERA_PERMISSION = 100
+        private val TAG = CameraFragment::class.qualifiedName
+        @JvmStatic fun newInstance() = CameraFragment()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions ,grantResults)
+    }
+
+    @AfterPermissionGranted(REQUEST_CAMERA_PERMISSION)
+    private fun checkCameraPermission() {
+        if (EasyPermissions.hasPermissions(activity!!, Manifest.permission.CAMERA)) {
+            Log.d(TAG, "App has camera permission")
+            connectCamera()
+        } else {
+            EasyPermissions.requestPermissions(activity!!,
+                getString(R.string.camera_request_rationale),
+                REQUEST_CAMERA_PERMISSION,
+                Manifest.permission.CAMERA)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        startBackgroundThread()
+        if (textureview.isAvailable)
+            openCamera()
+        else
+            textureview.surfaceTextureListener = surfaceListener
+    }
+
+    override fun onPause() {
+
+        stopBackgroundThread()
+        super.onPause()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        activity?.window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    }
+
+    private fun openCamera() {
+        checkCameraPermission()
+    }
+}
