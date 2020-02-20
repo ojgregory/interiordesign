143247f4770d2e58d6da8c22b485e4cf8c8744ac
Oliver Gregory <oliver.gregory@students.plymouth.ac.uk>
2020-02-20 14:52:42 +0000

    Added gaussian kernel calculation for usage in gaussian blur

---
+++ app/src/main/java/uk/ac/plymouth/interiordesign/Processors/GaussianCalculator.kt
@@ -0,0 +1,46 @@
+package uk.ac.plymouth.interiordesign.Processors
+
+import kotlin.math.exp
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class GaussianCalculator(sigma : Double, maskSize: Int) {
+    var mSigma : Double = sigma
+        get() = field
+        set(value) {
+            field = value
+            createGaussianKernel()
+        }
+    var mMaskSize : Int = maskSize
+        get() = field
+        set(value) {
+            field = value
+            kernel = DoubleArray(value)
+            createGaussianKernel()
+        }
+    var kernel = DoubleArray(maskSize)
+
+
+    fun createGaussianKernel() {
+        val constant = 1 / sqrt(2 * Math.PI * mSigma.pow(2))
+        val divisor = 2 * mSigma.pow(2)
+
+        var acc = 0.0
+
+        for  (i in 0 until mMaskSize) {
+            var x = i - (mMaskSize / 2)
+            kernel[i] = constant * exp(-(x * x) / divisor)
+            acc += kernel[i]
+        }
+
+        for (i in 0 until mMaskSize) {
+            kernel[i] = kernel[i] / acc
+            kernel[i] = Math.round(kernel[i] * 100000.0) / 100000.0
+        }
+
+    }
+
+
+
+
+}
