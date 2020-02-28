30e0646941bb7c95f8a8f5922bb0c8eb8a022fee
Oliver Gregory <oliver.gregory@students.plymouth.ac.uk>
2020-02-28 16:43:38 +0000

    Switched to correct sobel and scharr operators

--- app/src/main/java/uk/ac/plymouth/interiordesign/Processors/SobelOperatorProvider.kt
+++ app/src/main/java/uk/ac/plymouth/interiordesign/Processors/SobelOperatorProvider.kt
@@ -2,10 +2,10 @@ package uk.ac.plymouth.interiordesign.Processors
 
 class SobelOperatorProvider {
     val maskSize = 3
-    val sobelOperatorX = intArrayOf(3, 0, -3, 10, 0, -10, 3, 0, -3)
-    val sobelOperatorY = intArrayOf(3, 10, 3, 0, 0, 0, -3, -10, -3)
-    val scharrOperatorX = intArrayOf(47, 0, -47, 162, 0, -162, 47, 0, -47)
-    val scharrOperatorY = intArrayOf(47, 162, 47, 0, 0, 0, -47, -162, -47)
+    val scharrOperatorX = intArrayOf(3, 0, -3, 10, 0, -10, 3, 0, -3)
+    val scharrOperatorY = intArrayOf(3, 10, 3, 0, 0, 0, -3, -10, -3)
+    val sobelOperatorX = intArrayOf(1, 0, -1, 2, 0, -2, 1, 0, -1)
+    val sobelOperatorY = intArrayOf(1, 2, 1, 0, 0, 0, -1, -2, -1)
     var selectedOperator = 0
         set(value) {
             if (value in 0..1)
