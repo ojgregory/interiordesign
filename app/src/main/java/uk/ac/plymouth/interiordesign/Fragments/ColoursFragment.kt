8ae7ae2115cb852efc70954e861671ce55c8f110
Oliver Gregory <oliver.gregory@students.plymouth.ac.uk>
2020-04-22 16:28:11 +0100

    Colours are correctly loaded from Room database, and are persisted over multiple launches so permanent additions to colours are possible

--- app/src/main/java/uk/ac/plymouth/interiordesign/Fragments/ColoursFragment.kt
+++ app/src/main/java/uk/ac/plymouth/interiordesign/Fragments/ColoursFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_colours.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -18,6 +19,7 @@ import uk.ac.plymouth.interiordesign.Room.ColourDatabase
 
 class ColoursFragment : Fragment(), DataReturnInterface<Colour>{
     lateinit var colourReturnInterface: DataReturnInterface<Colour>
+    lateinit var colourAdapter : ColourAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,161 +30,93 @@ class ColoursFragment : Fragment(), DataReturnInterface<Colour>{
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val colours = mutableListOf<Colour>()
         val colourDao = ColourDatabase.getDatabase(requireContext()).colourDao()
-        var colourAdapter : ColourAdapter
         GlobalScope.launch {
-            colourDao.deleteAll()
-            colourDao.insert(
-                Colour(
-                    255,
-                    0,
-                    0,
-                    255,
-                    "RED"
+            if (colourDao.getAll().isEmpty()) {
+                colourDao.insert(
+                    Colour(
+                        255,
+                        0,
+                        0,
+                        255,
+                        "RED"
+                    )
                 )
-            )
-            colourDao.insert(
-                Colour(
-                    0,
-                    255,
-                    0,
-                    255,
-                    "GREEN"
+                colourDao.insert(
+                    Colour(
+                        0,
+                        255,
+                        0,
+                        255,
+                        "GREEN"
+                    )
                 )
-            )
-            colourDao.insert(
-                Colour(
-                    0,
-                    0,
-                    255,
-                    255,
-                    "BLUE"
+                colourDao.insert(
+                    Colour(
+                        0,
+                        0,
+                        255,
+                        255,
+                        "BLUE"
+                    )
                 )
-            )
-            colourDao.insert(
-                Colour(
-                    255,
-                    255,
-                    0,
-                    255,
-                    "YELLOW"
+                colourDao.insert(
+                    Colour(
+                        255,
+                        255,
+                        0,
+                        255,
+                        "YELLOW"
+                    )
                 )
-            )
-            colourDao.insert(
-                Colour(
-                    255,
-                    0,
-                    255,
-                    255,
-                    "MAGENTA"
+                colourDao.insert(
+                    Colour(
+                        255,
+                        0,
+                        255,
+                        255,
+                        "MAGENTA"
+                    )
                 )
-            )
-            colourDao.insert(
-                Colour(
-                    0,
-                    255,
-                    255,
-                    255,
-                    "CYAN"
+                colourDao.insert(
+                    Colour(
+                        0,
+                        255,
+                        255,
+                        255,
+                        "CYAN"
+                    )
                 )
-            )
-            colourDao.insert(
-                Colour(
-                    255,
-                    255,
-                    255,
-                    255,
-                    "WHITE"
+                colourDao.insert(
+                    Colour(
+                        255,
+                        255,
+                        255,
+                        255,
+                        "WHITE"
+                    )
                 )
-            )
-            colourDao.insert(
-                Colour(
-                    0,
-                    0,
-                    0,
-                    255,
-                    "BLACK"
+                colourDao.insert(
+                    Colour(
+                        0,
+                        0,
+                        0,
+                        255,
+                        "BLACK"
+                    )
                 )
-            )
+            }
+            val colours = colourDao.getAll()
+            GlobalScope.launch(Dispatchers.Main) {
+                launchAdapter(colours)
+            }
         }
-        colours.add(
-            Colour(
-                255,
-                0,
-                0,
-                255,
-                "RED"
-            )
-        )
-        colours.add(
-            Colour(
-                0,
-                255,
-                0,
-                255,
-                "GREEN"
-            )
-        )
-        colours.add(
-            Colour(
-                0,
-                0,
-                255,
-                255,
-                "BLUE"
-            )
-        )
-        colours.add(
-            Colour(
-                255,
-                255,
-                0,
-                255,
-                "YELLOW"
-            )
-        )
-        colours.add(
-            Colour(
-                255,
-                0,
-                255,
-                255,
-                "MAGENTA"
-            )
-        )
-        colours.add(
-            Colour(
-                0,
-                255,
-                255,
-                255,
-                "CYAN"
-            )
-        )
-        colours.add(
-            Colour(
-                255,
-                255,
-                255,
-                255,
-                "WHITE"
-            )
-        )
-        colours.add(
-            Colour(
-                0,
-                0,
-                0,
-                255,
-                "BLACK"
-            )
-        )
-            colourAdapter =
-                ColourAdapter(requireContext(), R.layout.fragment_colour, colours)
+    }
 
-            colourList.adapter = colourAdapter
-
+    private fun launchAdapter(colours : List<Colour>) {
+        colourAdapter =
+            ColourAdapter(requireContext(), R.layout.fragment_colour, colours)
+        colourList.adapter = colourAdapter
     }
 
     override fun onAttach(context: Context) {
